220baf45036c3587bdde53cf101b405cdeb255fa
Nikolay Samus <2345977@gmail.com>
2023-11-15 17:59:55 +0700

    Урок 6. Компонент. Частичный класс, страницы шаблона, ссылка на компонент.

---
+++ Pages/Counter.razor.cs
@@ -0,0 +1,21 @@
+﻿using GameIndustry.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace GameIndustry.Pages
+{
+    public partial class Counter
+    {
+
+        [Inject] SingletonService singletonService { get; set; }
+        [Inject] TransientService transientService { get; set; }
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+
+            singletonService.Value = currentCount;
+            transientService.Value = currentCount;
+        }
+    }
+}
